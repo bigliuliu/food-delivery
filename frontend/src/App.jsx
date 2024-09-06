f2d9f265dd34003a2e91cbed8fa4bf5617189aa2
bigliuliu <1552000425@qq.com>
2024-09-06 18:31:19 +0800

    init front end project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+const App = () => {
+  return (
+    <div className='app'>
+      <Navbar></Navbar>
+    </div>
+  )
+}
+
+export default App
