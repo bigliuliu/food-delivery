17ce6e5e06e187593b249a9c201d1d531bd5287d
bigliuliu <1552000425@qq.com>
2024-09-07 23:47:32 +0800

    added fond item styles

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './FoodItem.css'
 import { assets } from '../../assets/assets'
 const FoodItem = ({id,name,price,description,image}) => {
+    const [itemCount,setItemCount] = useState(0)
   return (
     <div className='food-item'>
       <div className="food-item-img-container">
         <img className='food-item-image' src={image} alt="" />
+        {
+            !itemCount ? <img src={assets.add_icon_white} onClick={} alt="" /> : <div className='food-item-counter'></div>
+        }
       </div>
       <div className="food-item-info">
         <div className="food-item-name-rating">
