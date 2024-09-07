2db5d67cc095344492032dd92d345fe3de8be789
bigliuliu <1552000425@qq.com>
2024-09-07 21:53:47 +0800

    added food list components to show top dishes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,9 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {BrowserRouter} from 'react-router-dom'
+import {StoreContextProvider} from './context/StoreContext.jsx'
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-  <App />
+  <StoreContextProvider><App /></StoreContextProvider>
   </BrowserRouter>
 )
