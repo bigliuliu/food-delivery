690ae1d932fcfffef9d83b0b0377b03e49912a60
bigliuliu <1552000425@qq.com>
2024-09-08 15:46:58 +0800

    added app download component and make fully responsive

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   const [menu, setMenu] = useState("home");
   return (
     <div className="navbar">
-      <img src={assets.logo} alt="" className="" />
+      <img src={assets.logo} alt="" className="logo" />
       <ul className="navbar-menu">
         <li onClick={()=>setMenu("home")} className={menu === "home"?"active":""}>home</li>
         <li onClick={()=>setMenu("menu")} className={menu === "menu"?"active":""}>menu</li>
