cc69eab05bb443a86313aa7e07c194ff36c6835e
bigliuliu <1552000425@qq.com>
2024-09-09 18:14:12 +0800

    fixed cart dot if shows or not also added total amount fee functions

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import "./Navbar.css";
 import { assets } from "../../assets/assets";
 import { Link } from "react-router-dom";
+import { StoreContext } from "../../context/StoreContext";
 const Navbar = ({ setShowLogin }) => {
   const [menu, setMenu] = useState("home");
+  const { getTotalCartAmount } = useContext(StoreContext);
   return (
     <div className="navbar">
       <Link to="/">
@@ -45,7 +47,8 @@ const Navbar = ({ setShowLogin }) => {
           <Link to="/cart">
             <img src={assets.basket_icon} alt="" />
           </Link>
-          <div className="dot"></div>
+
+          <div className={getTotalCartAmount()=== 0 ? "" : "dot"}></div>
         </div>
         <button onClick={() => setShowLogin(true)}>sign in</button>
       </div>
--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -18,15 +18,23 @@ const StoreContextProvider = ({children}) => {
             ...prev,[itemId]:prev[itemId]-1
         }))
     }
-    useEffect(()=>{
-        console.log(cartItems,"----------")
-    },[cartItems])
+    const getTotalCartAmount = ()=>{
+        let totalAmount = 0;
+        for(const item in cartItems){
+            if(cartItems[item]>0){
+                let itemInfo = food_list.find((product)=>product._id === item)
+                totalAmount+= itemInfo.price* cartItems[item]
+            }    
+        }
+        return totalAmount;
+    }
     const contextValue = {
         food_list,
         cartItems,
         setCartItems,
         addToCart,
-        removeFromCart
+        removeFromCart,
+        getTotalCartAmount
     }
 
     return(
