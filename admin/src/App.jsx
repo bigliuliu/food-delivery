07d9a1f4c1581506ae866e515cd0d3e145a381dc
bigliuliu <1552000425@qq.com>
2024-09-11 18:55:38 +0800

    Designed sidebar by add active class

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Sidebar from './components/Sidebar/Sidebar'
-
+import { Routes ,Route} from 'react-router-dom'
+import Add from './pages/Add/Add'
+import Orders from './pages/Orders/Orders'
+import List from './pages/List/List'
 const App = () => {
   return (
     <div>
@@ -9,6 +12,11 @@ const App = () => {
       <hr />
       <div className="app-content">
         <Sidebar/>
+        <Routes>
+            <Route path="/add" element={Add}/>
+            <Route path="/orders" element={Orders}/>
+            <Route path="/list" element={List}/>
+        </Routes>
       </div>
     </div>
   )
