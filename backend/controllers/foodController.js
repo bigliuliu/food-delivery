f550ad46eb483cc104565fb548249a6437539972
bigliuliu <1552000425@qq.com>
2024-09-11 16:27:51 +0800

    added remove food api

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -2,7 +2,6 @@ import foodModel from "../models/foodModel.js";
 import fs from "fs";
 
 // add food item
-
 const addFood = async (req, res) => {
   let image_filename = `${req.file.filename}`;
   const food = new foodModel({
@@ -17,7 +16,31 @@ const addFood = async (req, res) => {
     res.json({ success: true, message: "Food Added" });
   } catch (error) {
     console.log(error);
-    res.json({success:false, message:"Error"})
+    res.json({ success: false, message: "Error" });
   }
 };
-export { addFood };
+
+//  all food list
+const listFood = async (req, res) => {
+  try {
+    const foods = await foodModel.find({});
+    res.json({ success: true, data: foods });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+// remove food item
+const removeFood = async (req, res) => {
+  try {
+    const food = await foodModel.findById(req.body.id);
+    fs.unlink(`uploads/${food.image}`, () => {});
+    await foodModel.findByIdAndDelete(req.body.id);
+    res.json({ success: true, message: "Food removeFood" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+export { addFood, listFood, removeFood };
