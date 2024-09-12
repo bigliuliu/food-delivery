0cc06b2109e5ef37cb667e7781193bc5533b0a56
bigliuliu <1552000425@qq.com>
2024-09-12 18:45:31 +0800

    added login and register api functions

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,29 @@
 import express from "express";
-import cors from 'cors'
+import cors from "cors";
 import { connectDB } from "./config/db.js";
-import foodRouter from './routes/foodRoute.js';
-
+import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
+import 'dotenv/config.js'
 // app config
-const app = express()
-const port = 4000
+const app = express();
+const port = 4000;
 
 // middleware
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 // db connection
 connectDB();
 
 // api endpoints
-app.use("/api/food",foodRouter)
-app.use("/images",express.static('uploads'))
+app.use("/api/food", foodRouter);
+app.use("/images", express.static("uploads"));
+app.use("/api/user", userRouter);
 
-app.get("/",(req,res)=>{
-    res.send("API Working")
-})
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
 
-app.listen(port,()=>{
-    console.log(`Server started on http://localhost:${port}`)
-})
-
+app.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}`);
+});
--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -1,8 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./LoginPopup.css";
 import { assets } from "../../assets/assets";
 const LoginPopup = ({ setShowLogin }) => {
   const [currSate, setCurrState] = useState("Login");
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const onChangeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setData((data) => ({
+      ...data,
+      [name]: value,
+    }));
+  };
+  useEffect(()=>{
+    console.log(data,"-----------")
+  },[data])
   return (
     <div className="login-popup">
       <form action="" className="login-popup-container">
@@ -18,10 +34,10 @@ const LoginPopup = ({ setShowLogin }) => {
           {currSate === "Login" ? (
             <></>
           ) : (
-            <input type="text" placeholder="Your name" required />
+            <input name="name" onChange={onChangeHandler} value={data.name} type="text" placeholder="Your name" required />
           )}
-          <input type="email" placeholder="Your email" required />
-          <input type="password" placeholder="Password" required />
+          <input type="email" name="email" onChange={onChangeHandler} value={data.email} placeholder="Your email" required />
+          <input type="password" name="password" onChange={onChangeHandler} value={data.password} placeholder="Password" required />
         </div>
         <button>{currSate === "Sign Up" ? "Create account" : "Login"}</button>
         <div className="login-popup-condition">
@@ -30,11 +46,13 @@ const LoginPopup = ({ setShowLogin }) => {
         </div>
         {currSate === "Login" ? (
           <p>
-            Create a new account? <span onClick={()=>setCurrState("Sign Up")}>Click here</span>
+            Create a new account?{" "}
+            <span onClick={() => setCurrState("Sign Up")}>Click here</span>
           </p>
         ) : (
           <p>
-            Already have an account? <span onClick={()=>setCurrState("Login")}>Login here</span>
+            Already have an account?{" "}
+            <span onClick={() => setCurrState("Login")}>Login here</span>
           </p>
         )}
       </form>
