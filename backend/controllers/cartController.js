0fc892bcf884e9275460b438edaef889a215f8ee
bigliuliu <1552000425@qq.com>
2024-09-13 11:44:23 +0800

    added authoration for cart data

---
+++ backend/controllers/cartController.js
@@ -0,0 +1,12 @@
+import req from "express/lib/request.js";
+import userModel from "../models/userModel.js";
+
+// add items to user cart
+const addToCart = async (req, res) => {};
+
+// remove items from user cart
+const removeFromCart = async (req, res) => {};
+
+// fetch user cart data
+const getCart = async (req, res) => {};
+export { addToCart, removeFromCart, getCart };
