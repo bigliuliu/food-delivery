736e6735850763f1d3e9ca0ec9479a589d02fbf2
bigliuliu <1552000425@qq.com>
2024-09-14 17:16:23 +0800

    implement an api add food to cart

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -17,6 +17,7 @@ const loginUser = async (req, res) => {
     const token = createToken(user._id);
     res.json({ success: true, token });
   } catch (error) {
+    console.log(error)
     res.json({ success: false, message: "Error" });
   }
 };
@@ -60,6 +61,7 @@ const registerUser = async (req, res) => {
     const token = createToken(user._id);
     res.json({ success: true, token });
   } catch (error) {
+    console.log(error)
     res.json({ success: false, message: "Error" });
   }
 };
--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { addToCart,removeFromCart,getCart } from "../controllers/cartController.js";
 import authMiddleware from "../middleware/auth.js";
+
 const cartRouter = express.Router()
 
 cartRouter.post("/add",authMiddleware,addToCart)
