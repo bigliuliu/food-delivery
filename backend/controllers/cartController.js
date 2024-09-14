6b8b41aefe3b4677ecddf59e1e547b72a5ba605b
bigliuliu <1552000425@qq.com>
2024-09-14 17:37:12 +0800

    fixed an error spell wrong

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -2,42 +2,23 @@ import req from "express/lib/request.js";
 import userModel from "../models/userModel.js";
 
 // add items to user cart
-// const addToCart = async (req, res) => {
-//   try {
-//     let userData = await userModel.findById(req.body.userId);
-//     console.log(req,"------------")
-//     let cartData = await userData.cartData;
-//     if (!cartData[req.body.itemId]) {
-//       cartData[req.body.itemId] = 1;
-//     } else {
-//       cartData[req.body.itemId] += 1;
-//     }
-//     await userModel.findByIdAndUpdate(req.boby.userId, { cartData });
-//      res.json({ success: true, message: "Added to cart" });
-//   } catch (error) {
-//     console.log(error)
-//     res.json({ success: false, message: "Error" })
-//   }
-// };
-
-const addToCart = async (req,res) => {
-    try {
-        let userData = await userModel.findById(req.body.userId);
-        let cartData = await userData.cartData;
-        if (!cartData[req.body.itemId])
-        {
-            cartData[req.body.itemId] = 1;
-        }
-        else{
-            cartData[req.body.itemId] += 1;
-        }
-        await userModel.findByIdAndUpdate(req.body.userId,{cartData})
-        res.json({success:true,message:"Added to cart"});
-    } catch (error) {
-        console.log(error);
-        res.json({success:false,message:"Error"})
+const addToCart = async (req, res) => {
+  try {
+    let userData = await userModel.findById(req.body.userId);
+    console.log(req, "------------");
+    let cartData = await userData.cartData;
+    if (!cartData[req.body.itemId]) {
+      cartData[req.body.itemId] = 1;
+    } else {
+      cartData[req.body.itemId] += 1;
     }
-}
+    await userModel.findByIdAndUpdate(req.body.userId, { cartData });
+    res.json({ success: true, message: "Added to cart" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
 
 // remove items from user cart
 const removeFromCart = async (req, res) => {};
