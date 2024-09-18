dbf4753f003ce201b084e442fc5552153a9e126c
bigliuliu <1552000425@qq.com>
2024-09-18 18:58:31 +0800

    added router to route my orders

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -62,14 +62,14 @@ const Navbar = ({ setShowLogin }) => {
           <div className="navbar-profile">
             <img src={assets.profile_icon} alt="" />
             <ul className="nav-profile-dropdown">
-              <li>
+              <li onClick={()=>navigate('/myorders')}>
                 <img src={assets.bag_icon} alt="" />
                 <p>Orders</p>
               </li>
               <hr />
-              <li>
+              <li onClick={()=>logout()}>
                 <img src={assets.logout_icon} alt="" />
-                <p onClick={()=>logout()}>Logout</p>
+                <p >Logout</p>
               </li>
             </ul>
           </div>
