de673d05bed05fc8eaca866f60c5e038e8cc6882
bigliuliu <1552000425@qq.com>
2024-09-19 15:37:31 +0800

    integretated api to update order status

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -13,6 +13,14 @@ const Orders = ({ url }) => {
       toast.error("Error");
     }
   };
+  const statusHandler = async (event,orderId)=>{
+    console.log(event.target.value)
+    const response = await axios.post(url+"/api/order/status",{orderId,status:event.target.value})
+    if(response.data.success){
+      await fetchAllOrders()
+    }
+  }
+
   useEffect(() => {
     fetchAllOrders();
   }, []);
@@ -54,7 +62,7 @@ const Orders = ({ url }) => {
                 </div>
                 <p>Items : {order.items.length}</p>
                 <p>${order.amount}</p>
-                <select>
+                <select onChange={(event)=>statusHandler(event,order._id)} value={order.status}>
                   <option value="Food Processing">Food Processing</option>
                   <option value="Out for delivery">Out for delivery</option>
                   <option value="Deliveried">Deliveried</option>
