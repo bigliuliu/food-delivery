f1b86cc9316b66cd735119d35edae49f9144e88a
bigliuliu <1552000425@qq.com>
2024-09-19 17:36:40 +0800

    updated living server url

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -8,7 +8,7 @@ import List from './pages/List/List'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 const App = () => {
-  const url = "http://localhost:4000"
+  const url = "https://food-delivery-backend-7eqb.onrender.com"
   return (
     <div>
       <ToastContainer/>
