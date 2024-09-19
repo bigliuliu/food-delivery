1abfe428f54623228d3f3e640ac37e3ce271bfa6
bigliuliu <1552000425@qq.com>
2024-09-19 16:56:35 +0800

    updated server port

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import 'dotenv/config.js'
 import orderRouter from "./routes/orderRoute.js";
 // app config
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 // middleware
 app.use(express.json());
