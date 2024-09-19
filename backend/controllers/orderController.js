37de3629aab642db9d8497513b80dfc668196db1
bigliuliu <1552000425@qq.com>
2024-09-19 18:15:56 +0800

    updated frontend_url

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // placing user order for frontend
 const placeOrder = async (req, res) => {
-  const frontend_url = "http://localhost:5173";
+  const frontend_url = "https://food-delivery-frontend-8kro.onrender.com";
   try {
     const newOrder = new orderModel({
       userId: req.body.userId,
