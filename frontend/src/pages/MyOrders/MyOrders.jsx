db29fa9ed79e116bcb3ccadaf3eac1e6affa125c
bigliuliu <1552000425@qq.com>
2024-09-19 15:43:31 +0800

    added a button event to update order status

--- frontend/src/pages/MyOrders/MyOrders.jsx
+++ frontend/src/pages/MyOrders/MyOrders.jsx
@@ -47,7 +47,7 @@ const MyOrders = () => {
                   <span>&#x25cf;</span>
                   <b>{order.status}</b>
                 </p>
-                <button>Track Order</button>
+                <button onClick={fetchOrders}>Track Order</button>
               </div>
             );
           }
